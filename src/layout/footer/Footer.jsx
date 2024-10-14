508607bfdadeb8bedeb1ee238894d4ce7f94ba98
JanivDemagniv <janivjry@gmail.com>
2024-10-14 12:59:30 +0300

    cleaning footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -4,15 +4,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <Box sx={{ width: '100%', bgcolor: '#232F34', minHeight: '20vh', p: "30px" }}>
-            <Paper sx={{ m: '10px' }}>
-                About
-            </Paper>
-            <Paper sx={{ m: '10px' }}>
-                What
-            </Paper>
-            <Paper sx={{ m: '10px' }}>
-                Yo
-            </Paper>
+
         </Box>
     )
 }
