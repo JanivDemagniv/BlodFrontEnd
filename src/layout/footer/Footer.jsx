305134d5093ed2dc49473288e7589ec365cd5cf5
JanivDemagniv <janivjry@gmail.com>
2024-10-14 15:14:51 +0300

    update name on footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -3,37 +3,41 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <Box sx={{
-            width: '100%',
-            bgcolor: '#232F34',
-            minHeight: '20vh',
-            p: "2% 20%",
-            display: 'flex',
-            gap: '5%',
-            color: '#fff'
-        }}>
-            <List dense>
-                <Typography sx={{ textDecoration: 'underLine' }}>Navigate</Typography>
-                <ListItem>
-                    <ListItemText primary="Home" />
-                </ListItem>
-                <ListItem>
-                    <ListItemText primary='Posts' />
-                </ListItem>
-                <ListItem>
-                    <ListItemText primary='Join Us' />
-                </ListItem>
-            </List>
-            <List dense>
-                <Typography sx={{ textDecoration: 'underLine' }}>About Us</Typography>
-                <ListItem>
-                    <ListItemText>
-                        We love music , espicelly albums<br />
-                        we cheriche this site tot brig you<br />
-                        all reviews of all albums
-                    </ListItemText>
-                </ListItem>
-            </List>
+        <Box sx={{ bgcolor: '#232F34', }}>
+            <Box sx={{
+                width: '100%',
+                minHeight: '20vh',
+                p: "2% 20%",
+                display: 'flex',
+                gap: '5%',
+                color: '#fff'
+            }}>
+                <List dense>
+                    <Typography sx={{ textDecoration: 'underLine' }}>Navigate</Typography>
+                    <ListItem>
+                        <ListItemText primary="Home" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary='Posts' />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary='Join Us' />
+                    </ListItem>
+                </List>
+                <List dense>
+                    <Typography sx={{ textDecoration: 'underLine' }}>About Us</Typography>
+                    <ListItem>
+                        <ListItemText>
+                            We love music , espicelly albums<br />
+                            we cheriche this site tot brig you<br />
+                            all reviews of all albums
+                        </ListItemText>
+                    </ListItem>
+                </List>
+            </Box>
+            <Box>
+                <Typography sx={{ display: 'flex', justifyContent: 'flex-end', p: '0 2%', color: '#fff' }}>Yaniv Romem</Typography>
+            </Box>
         </Box>
     )
 }
