9f3a60d6b623fe0defb119b51134516be7eaa902
JanivDemagniv <janivjry@gmail.com>
2024-10-23 13:23:09 +0300

    user menu

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import LogoIcon from './logo/LogoIcon'
 import Logo from './logo/Logo'
 import LeftNavBar from './leftNav/LeftNavBar'
+import ProfileManu from './rightNav/ProfileManu'
 
 export default function Navbar() {
     return (
         <AppBar position='sticky'>
-            <Toolbar>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
                 <LeftNavBar />
+                <ProfileManu />
             </Toolbar>
         </AppBar>
     )
---
+++ src/layout/navbar/rightNav/ProfileManu.jsx
@@ -0,0 +1,43 @@
+import { Avatar, Box, IconButton, Menu, MenuItem, Tooltip, Typography } from '@mui/material';
+import React from 'react'
+import { useState } from 'react'
+
+const settings = ['profile', 'my posts', 'login', 'logout']
+
+export default function ProfileManu() {
+    const [anchorElUser, setAnchorElUser] = useState(null);
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+            >
+                <MenuItem>Profile</MenuItem>
+            </Menu>
+        </Box>
+    )
+}
