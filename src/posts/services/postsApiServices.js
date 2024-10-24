b29b6343dccf017f6edaf67d3f5e4b38883b4d07
JanivDemagniv <janivjry@gmail.com>
2024-10-24 14:06:55 +0300

    first api servies

---
+++ src/posts/services/postsApiServices.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+export const apiUrl = 'http://localhost:8181/posts/';
+
+export const getAllPosts = async () => {
+    try {
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+        return data
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
