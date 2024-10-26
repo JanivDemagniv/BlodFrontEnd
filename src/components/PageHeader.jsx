c45aaca8c70d132a40b89e86b0c25e7183626ef8
JanivDemagniv <janivjry@gmail.com>
2024-10-26 18:12:12 +0300

    more darkmode updates

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,9 +1,12 @@
 import { Box, Divider, Typography } from '@mui/material'
 import React from 'react'
+import { useTheme } from '../providers/CustomTheme'
 
 export default function PageHeader({ title, subtitle }) {
+    const { isDark } = useTheme();
+
     return (
-        <Box sx={{ p: '20px 0 5px' }}>
+        <Box sx={{ p: '20px 0 5px', color: isDark ? '#fff' : '#000' }}>
             <Typography align='center' variant='h2' component='h1'>{title}</Typography>
             <Typography align='center' variant='h5' component='h2'>{subtitle}</Typography>
             <Divider sx={{ my: 2 }} />
