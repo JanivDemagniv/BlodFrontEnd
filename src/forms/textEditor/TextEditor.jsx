31242dea9481a1de2c1ee94b3d591a0a9ff85b27
JanivDemagniv <janivjry@gmail.com>
2024-10-28 15:45:30 +0200

    styles for texteditor

--- src/forms/textEditor/TextEditor.jsx
+++ src/forms/textEditor/TextEditor.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import { Box } from '@mui/material';
+import React from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css';
 
@@ -7,10 +8,10 @@ export default function TextEditor({
     onChange,
     data
 }) {
-    const [value, setValue] = useState();
-
 
     return (
-        <ReactQuill value={data.content} onChange={(e) => { onChange(e); setValue(p => p) }} />
+        <Box sx={{ m: '5px', width: '100%', border: '1px solid #bbb', minHeight: '50vh', borderRadius: '5px' }}>
+            <ReactQuill value={data.content} onChange={(e) => { onChange(e); setValue(p => p) }} />
+        </Box>
     )
 }
