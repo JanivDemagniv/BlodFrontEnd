427f5d203757adcb8f14cdccbb8d1a84770c4fde
JanivDemagniv <janivjry@gmail.com>
2024-11-05 21:48:03 +0200

    datagrid color

--- src/crm/components/AllUsers.jsx
+++ src/crm/components/AllUsers.jsx
@@ -5,15 +5,17 @@ import Error from '../../components/Error';
 import { Avatar, Box, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import ActionsButtons from './components/ActionsButtons';
+import { useTheme } from '../../providers/CustomTheme';
 
 export default function AllUsers() {
     const { handleGetAllUsers, handleUpdateStatus, handleDeleteUser, error, isLoading, allUsers } = useAdmin();
+    const { isDark } = useTheme();
 
     const columns = [
-        { field: 'id', headerName: 'Id', width: 250, disableColumnMenu: true },
-        { field: 'username', headerName: 'Username', width: 200, disableColumnMenu: true },
+        { field: 'id', headerName: 'Id', width: 250, disableColumnMenu: true, headerClassName: 'header-color' },
+        { field: 'username', headerName: 'Username', width: 200, disableColumnMenu: true, headerClassName: 'header-color' },
         {
-            field: 'Avatar', headerName: 'Avatar', width: 60, disableColumnMenu: true,
+            field: 'Avatar', headerName: 'Avatar', width: 60, disableColumnMenu: true, headerClassName: 'header-color',
             renderCell: (params) => {
                 return (
                     <Box sx={{ p: '2px' }}>
@@ -21,15 +23,18 @@ export default function AllUsers() {
                     </Box>)
             }
         },
-        { field: 'first', headerName: 'First Name', width: 130, disableColumnMenu: true },
-        { field: 'middle', headerName: 'Middle Name', width: 130, disableColumnMenu: true },
-        { field: 'last', headerName: 'Last Name', width: 130, disableColumnMenu: true },
-        { field: 'isCreator', header: 'Creator', width: 130, disableColumnMenu: true, },
+        { field: 'first', headerName: 'First Name', width: 130, disableColumnMenu: true, headerClassName: 'header-color' },
+        { field: 'middle', headerName: 'Middle Name', width: 130, disableColumnMenu: true, headerClassName: 'header-color' },
+        { field: 'last', headerName: 'Last Name', width: 130, disableColumnMenu: true, headerClassName: 'header-color' },
+        {
+            field: 'isCreator', header: 'Creator', width: 130, disableColumnMenu: true, headerClassName: 'header-color'
+        },
         {
             field: 'actions',
             type: 'actions',
             headerName: 'Actions',
             width: 100,
+            headerClassName: 'header-color',
             renderCell: (params) => {
                 return [
                     < ActionsButtons key={params.id} handleDeleteUser={handleDeleteUser} handleUpdateStatus={handleUpdateStatus} userId={params.id} />
@@ -56,7 +61,11 @@ export default function AllUsers() {
                     columns={columns}
                     initialState={{ pagination: { paginationModel } }}
                     pageSizeOptions={[5, 10, 25, 50, 100]}
-                    sx={{ border: 0 }}
+                    sx={{
+                        border: 0, '& .header-color': {
+                            backgroundColor: isDark ? '#607D8B' : '#ed6c02',
+                        }
+                    }}
                     editMode='row'
                     disableRowSelectionOnClick
                 />
