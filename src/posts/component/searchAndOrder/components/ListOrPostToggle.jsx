24023295133e1952b8e037408d9f68105878dea7
JanivDemagniv <janivjry@gmail.com>
2024-11-10 15:42:39 +0200

    undo changes

--- src/posts/component/searchAndOrder/components/ListOrPostToggle.jsx
+++ src/posts/component/searchAndOrder/components/ListOrPostToggle.jsx
@@ -3,15 +3,9 @@ import React from 'react'
 
 export default function ListOrPostToggle({ isList, handleToggle, sx }) {
     return (
-        <Box>
-            <FormControlLabel sx={{ ...sx }}
-                control={<Switch onClick={handleToggle} />}
-                label={isList ? 'Posts' : 'List'}
-            />
-            <FormControlLabel sx={{ ...sx }}
-                control={<Switch onClick={handleToggle} />}
-                label={isList ? 'Posts' : 'List'}
-            />
-        </Box>
+        <FormControlLabel sx={{ ...sx }}
+            control={<Switch onClick={handleToggle} />}
+            label={isList ? 'Posts' : 'List'}
+        />
     )
 }
